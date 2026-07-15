7ec00195352fcde56bd5ae4cbf568224add78731
MeoU_Ne <ducson9112k@gmail.com>
2026-07-16 00:20:53 +0700

    Resolve MIDP resources from program volume

--- src/flint/midp/ResourceLoader.java
+++ src/flint/midp/ResourceLoader.java
@@ -10,8 +10,9 @@ import java.io.InputStream;
  * <p>This is a FlintOS implementation class, not a MIDP public API.</p>
  */
 public final class ResourceLoader {
-    private static final String RESOURCE_ROOT = "/res";
+    private static final String FLASH_RESOURCE_ROOT = "/res";
     private static String suiteDirectory;
+    private static String resourceRoot;
 
     private ResourceLoader() {
     }
@@ -41,13 +42,39 @@ public final class ResourceLoader {
 
         String relativeName = name.startsWith("/") ? name.substring(1) : name;
         String currentSuiteDirectory = suiteDirectory;
+        String currentResourceRoot = getResourceRoot();
         String path;
         if(currentSuiteDirectory == null || currentSuiteDirectory.length() == 0) {
-            path = RESOURCE_ROOT + "/" + relativeName;
+            path = currentResourceRoot + "/" + relativeName;
         }
         else {
-            path = RESOURCE_ROOT + "/" + currentSuiteDirectory + "/" + relativeName;
+            path = currentResourceRoot + "/" + currentSuiteDirectory + "/" + relativeName;
         }
         return new FileInputStream(path);
     }
+
+    private static synchronized String getResourceRoot() {
+        if(resourceRoot == null)
+            resourceRoot = resourceRootForProgram(getProgramPath());
+        return resourceRoot;
+    }
+
+    static String resourceRootForProgram(String programPath) {
+        if(programPath == null || !programPath.startsWith("/mnt/sd"))
+            return FLASH_RESOURCE_ROOT;
+
+        int volumeEnd = programPath.indexOf('/', 7);
+        if(volumeEnd < 0)
+            return FLASH_RESOURCE_ROOT;
+        for(int i = 7; i < volumeEnd; i++) {
+            char character = programPath.charAt(i);
+            if(character < '0' || character > '9')
+                return FLASH_RESOURCE_ROOT;
+        }
+        if(volumeEnd == 7)
+            return FLASH_RESOURCE_ROOT;
+        return programPath.substring(0, volumeEnd) + "/res";
+    }
+
+    private static native String getProgramPath();
 }
